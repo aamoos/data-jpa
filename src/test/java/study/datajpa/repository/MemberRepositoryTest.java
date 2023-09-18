d4a4a33c9d54eaa18ccd9b0696a165c8e50f8bfb
aamoos <aamoos@naver.com>
2023-09-18 21:55:27 +0900

    JPA HINT

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -245,6 +245,35 @@ class MemberRepositoryTest {
             System.out.println("member.getTeam().getClass() = " + member.getTeam().getClass());
             System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
         }
+    }
 
+    @Test
+    public void queryhHint(){
+
+        //given
+        Member member1 = memberRepository.save(new Member("member1", 10));;
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
+    @Test
+    public void lock() {
+
+        //given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> result = memberRepository.findLockByUsername("member1");
+
+        em.flush();
     }
 }
